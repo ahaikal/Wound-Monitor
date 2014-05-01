8feebe4ba49f2e733d90984d15da6a85e78c701b
OsagieDev <OsagieO@gmail.com>
2014-05-01 13:31:45 -0500

    Some tests before pull

--- wounds_app/spec/user/user_spec.rb
+++ wounds_app/spec/user/user_spec.rb
@@ -2,9 +2,25 @@ require "spec_helper"
 
 describe User do
   it "should have a first name and second name" do
-    user = User.create!(first_name: "Andy", last_name: "Lindeman")
+    user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@lindeman.com", password: "12345A")
 
-    expect(User.first_name).to eq("Andy")
-    expect(User.first_name).to eq("Lindeman")
+    expect(user.first_name).to eq("Andy")
+    expect(user.last_name).to eq("Lindeman")
+    expect(user.name).to eq("Andy Lindeman")
   end
+
+  it "should be of type User" do
+    user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@lindeman.com", password: "12345A")
+    expect(user).to be_an_instance_of(User)
+  end
+
+  it "should not authenticate with wrong password" do
+    user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@lindeman.com", password: "12345A")
+    expect(user.authenticate("wrongpass")).should be_nil
+  end
+
+  it "should have secure password" do
+    it { should have_secure_password }
+  end
+
 end
