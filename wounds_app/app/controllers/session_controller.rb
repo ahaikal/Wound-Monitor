8e9842bc09ea30f870f50001d293ced992799f5c
jaybobo <info@goodproduce.net>
2014-05-01 10:38:50 -0500

    Big commit.
    Created routes for users, patients, wounds, and statuses.
    
    Created session controller with helper methods.

---
+++ wounds_app/app/controllers/session_controller.rb
@@ -0,0 +1,35 @@
+class SessionController < ApplicationController
+
+	def index
+		if logged_in?
+			@user = User.find(session[:user_id])
+			redirect_to patients_path
+		else
+			@user = User.new
+			render "index"
+		end
+	end
+
+
+  def create
+  	@user = User.find_by(email: user_params[:email])
+  	if @user && @user.authenticate(user_params[:password])
+  		session[:user_id] = @user.id
+  		redirect_to patients_path
+  	else
+  		redirect_to root_url
+  	end
+  end
+
+  def destroy
+  	session.clear
+  	redirect_to root_url
+  end
+
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :password)
+  end
+end
