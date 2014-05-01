cb49cbbae27251d90df9026a192cc2811b5c5250
Alan Haikal <alanhaikal@gmail.com>
2014-05-01 10:06:40 -0500

    added all migrations

--- wounds_app/db/migrate/20140501004208_create_wound.rb
+++ wounds_app/db/migrate/20140501004208_create_wound.rb
@@ -1,6 +1,10 @@
 class CreateWound < ActiveRecord::Migration
   def change
     create_table :wounds do |t|
+    	t.string	:location
+    	t.belongs_to	:patient
+
+    	t.timestamps
     end
   end
 end
