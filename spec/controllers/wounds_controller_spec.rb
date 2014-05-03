1323f551666f529a7deeadf5aae14e3b5867718f
jaybobo <info@goodproduce.net>
2014-05-03 16:30:54 -0500

    updated wound spec

--- spec/controllers/wounds_controller_spec.rb
+++ spec/controllers/wounds_controller_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe WoundsController do
-	
+	describe "GET #show" do
+		before :each do
+			@wound = FactoryGirl.create(:wound, :id => 1)
+			get :show, { :id => 1 }, {}
+		end
+
+		it "should render show template" do
+			should render_template('show')
+		end
+
+		it "should find Wound with id of 1" do
+			expect(@wound.id).to be 1
+		end
+	end
+
+	describe "GET #new" do
+		before { get :new }
+		it "should render new template" do
+			should render_template('new')
+		end
+	end
+
+	describe "POST #create" do
+		context "permits location & patient id" do
+			it { should permit(:location, :patient_id).for(:wound)}
+		end
+	end
 
 end
