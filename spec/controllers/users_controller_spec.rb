ff6516dbc49d0dfab5721e75d7dc7171bc14e02f
jaybobo <info@goodproduce.net>
2014-05-03 14:16:08 -0500

    Completed initial user_controller_spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe UsersController do
+
+	describe "GET #show" do
+		
+		before :each do
+			get :show, {}, { :user_id => 1 }
+		end
+
+		it "renders the show template" do
+			should render_template('show')
+		end
+
+		it "should set session to 1" do
+			should set_session(:user_id).to(1)
+		end
+	end
+end
