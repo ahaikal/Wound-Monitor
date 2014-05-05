292571a1b77b8b914a61587add3e973241ba6f8b
jaybobo <info@goodproduce.net>
2014-05-05 16:58:51 -0500

    added s3creds for public viewing, sike

--- app/models/status.rb
+++ app/models/status.rb
@@ -6,10 +6,16 @@ class Status < ActiveRecord::Base
   has_attached_file :image, 
   	:storage => :s3,
   	:bucket => ENV['AWS_BUCKET'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
   	:style => {
   	thumb: '150x150>',
   	medium: '300x300>'},
   	:path => "app/public/system/images/:id/:style/:basename.:extension"
+
+
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
