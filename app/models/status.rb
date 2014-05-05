45d25cd5324df252d698b8ef94f68c27be71f115
jaybobo <info@goodproduce.net>
2014-05-05 16:48:40 -0500

    added path and storage attribute

--- app/models/status.rb
+++ app/models/status.rb
@@ -4,11 +4,12 @@ class Status < ActiveRecord::Base
   belongs_to :patient
   
   has_attached_file :image, 
+  	:storage => :s3,
   	:bucket => ENV['AWS_BUCKET'],
   	:style => {
   	thumb: '150x150>',
-  	medium: '300x300>'
-  }
+  	medium: '300x300>'},
+  	:path => "app/public/system/images/:id/:style/:basename.:extension"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
