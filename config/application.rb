1a053e9d8189c55d870e43d02f3cd2d929efd1b2
jaybobo <info@goodproduce.net>
2014-05-06 10:57:32 -0500

    modified fonts paths

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module WoundsApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts','lato')
     config.assets.paths << Rails.root.join('app', 'assets', 'images')
     
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
