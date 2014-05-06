ad19bc0b694115859889a9031437da3732294e91
Alan Haikal <alanhaikal@gmail.com>
2014-05-06 18:03:05 -0500

    added API funcs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :sessions, :patients, :statuses
+      resources :sessions, :patients, :statuses, :users, :wounds
     end
   end
 
