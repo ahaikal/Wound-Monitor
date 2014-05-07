07a6e4b56c6e2ca87f322a253d91e06c0e8db5f3
jaybobo <info@goodproduce.net>
2014-05-07 01:10:34 -0500

    fixed static photo for missing images

--- app/models/status.rb
+++ app/models/status.rb
@@ -11,6 +11,7 @@ class Status < ActiveRecord::Base
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
   	:style => { :thumb => '150x150#', :medium => '300x300>'},
+    :default_url => 'original/missing.png',
   	:path => "app/public/system/images/:id/:style/:basename.:extension"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
